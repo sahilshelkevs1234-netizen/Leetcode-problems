003b59ab548c08e439d12827e9f3ec4219cd18ca
sahilshelkevs1234-netizen <sahilshelkevs1234@gmail.com>
2026-08-19 16:45:07 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (91.96%) - LeetSync

--- 670-maximum-swap/maximum-swap.cpp
+++ 670-maximum-swap/maximum-swap.cpp
@@ -5,17 +5,17 @@ public:
         string s = to_string(num);
         vector<int> last(10, -1);
         
-        // Record the last position of each digit 0-9
+       
         for (int i = 0; i < s.length(); ++i) {
             last[s[i] - '0'] = i;
         }
         
-        // Find the first digit that can be swapped with a larger digit appearing later
+       
         for (int i = 0; i < s.length(); ++i) {
             for (int d = 9; d > s[i] - '0'; --d) {
                 if (last[d] > i) {
-                    std::swap(s[i], s[last[d]]);
-                    return std::stoi(s);
+                    swap(s[i], s[last[d]]);
+                    return stoi(s);
                 }
             }
         }
